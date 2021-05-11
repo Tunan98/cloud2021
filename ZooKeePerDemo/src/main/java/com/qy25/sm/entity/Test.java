d15e17b30cd54e1476ebc249087d35f26f79b5d5
tunan <ts1916767131@163.com>
2021-05-11 22:52:00 +0800

    master-commit

--- ZooKeePerDemo/src/main/java/com/qy25/sm/entity/Test.java
+++ ZooKeePerDemo/src/main/java/com/qy25/sm/entity/Test.java
@@ -5,5 +5,6 @@ public class Test {
         System.out.println("1111111111");
         System.out.println("1111111111");
         System.out.println("1111111111");
+        System.out.println("master-test");
     }
 }
